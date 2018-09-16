dbc8acdf8217194b51ba9688eb5a933e6c874ef3
Safiulla-Safi <safiullabokaul@gmail.com>
2018-09-16 11:50:15 +0600

    add successfully all

---
+++ app/src/main/java/com/example/safi/testretrofit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.safi.testretrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
